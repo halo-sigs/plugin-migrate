debc1762bf64e187e02e33ea6fc18dfb37e18b51
Ryan Wang <i@ryanc.cc>
2022-10-19 17:52:08 +0800

    chore: update entry file

--- console/src/index.ts
+++ console/src/index.ts
@@ -1,33 +1,34 @@
-import { definePlugin, BasicLayout } from "@halo-dev/console-shared";
+import { definePlugin } from "@halo-dev/console-shared";
 import MigrateView from "./views/MigrateView.vue";
 import { IconGrid } from "@halo-dev/components";
+import { markRaw } from "vue";
 
 export default definePlugin({
   name: "PluginMigrate",
   components: [],
   routes: [
     {
-      path: "/migrate",
-      component: BasicLayout,
-      children: [
-        {
-          path: "",
-          name: "Migrate",
-          component: MigrateView,
-        },
-      ],
-    },
-  ],
-  menus: [
-    {
-      name: "系统",
-      items: [
-        {
-          name: "数据迁移",
-          path: "/migrate",
-          icon: IconGrid,
-        },
-      ],
+      parentName: "Root",
+      route: {
+        path: "/migrate",
+        children: [
+          {
+            path: "",
+            name: "Migrate",
+            component: MigrateView,
+            meta: {
+              title: "迁移",
+              searchable: true,
+              menu: {
+                name: "迁移",
+                group: "tool",
+                icon: markRaw(IconGrid),
+                priority: 0,
+              },
+            },
+          },
+        ],
+      },
     },
   ],
   extensionPoints: {},

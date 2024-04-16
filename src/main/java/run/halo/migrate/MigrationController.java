384ccb184dc506f1c0d92096322a554819965ba3
Takagi <1103069291@qq.com>
2024-04-16 20:55:25 +0800

    fix: solve the buffer error problem when parsing rss files (#36)
    #### What type of PR is this?
    
    /kind bug
    
    #### What this PR does / why we need it:
    
    此 RP 修复了由于解析 RSS 文件时缓存区未及时释放而导致缓存区溢出并报错的问题。
    
    #### How to test it?
    
    测试使用 rss 导入 https://yeung.xin/feed ，查看是否会出现报错。
    
    #### Which issue(s) this PR fixes:
    
    Fixes #35 
    
    #### Does this PR introduce a user-facing change?
    ```release-note
    解决导入 RSS 文件时缓存区可能溢出的问题
    ```

--- src/main/java/run/halo/migrate/MigrationController.java
+++ src/main/java/run/halo/migrate/MigrationController.java
@@ -5,8 +5,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferUtils;
@@ -16,9 +16,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import run.halo.app.plugin.ApiVersion;
 
@@ -36,27 +36,26 @@ public class MigrationController {
     })
     @Operation(operationId = "ParseRss", description = "parse rss url")
     @PostMapping(value = "rss-parse", consumes = MediaType.APPLICATION_JSON_VALUE)
-    Mono<String> rssParse(@RequestBody String url) {
+    Flux<String> rssParse(@RequestBody String url) {
         if (StringUtils.isBlank(url)) {
-            return Mono.empty();
+            return Flux.empty();
         }
-        try {
-            UriComponents uriComponents = UriComponentsBuilder.fromUriString(
-                url).build();
-            WebClient webClient = WebClient.create();
-            return webClient.get()
-                .uri(uriComponents.toUri())
-                .retrieve()
-                .bodyToFlux(DataBuffer.class)
-                .reduce(DataBuffer::write)
-                .map(buffer -> {
+        UriComponents uriComponents = UriComponentsBuilder
+            .fromUriString(url)
+            .build();
+        WebClient webClient = WebClient.create();
+        return webClient.get()
+            .uri(uriComponents.toUri())
+            .retrieve()
+            .bodyToFlux(DataBuffer.class)
+            .flatMap(buffer -> {
+                try {
                     byte[] bytes = new byte[buffer.readableByteCount()];
                     buffer.read(bytes);
+                    return Mono.just(new String(bytes, StandardCharsets.UTF_8));
+                } finally {
                     DataBufferUtils.release(buffer);
-                    return new String(bytes, StandardCharsets.UTF_8);
-                });
-        } catch (Exception e) {
-            return Mono.empty();
-        }
+                }
+            });
     }
 }
